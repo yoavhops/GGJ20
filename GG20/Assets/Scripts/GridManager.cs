9f6d7a04d9847ad0f9559678a00d0ebd2ba2b211
yoavhops <yoav.hops@gmail.com>
2020-01-31 15:05:27 +0200

    First Commit

---
+++ GG20/Assets/Scripts/GridManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public static GridManager Singleton;
+    public int Width = 100;
+    public int Height = 100;
+    public float[,] Grid;
+
+    void Awake()
+    {
+        Singleton = this;
+        Grid = new float[Width, Height];
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
