fbdd47a989d225340d70f1525cd6d7ef3a66b2c5
shirborer <shirborer@gmail.com>
2020-02-01 14:26:38 +0000

    a

---
+++ GG20/Assets/Scripts/CarbonPointHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarbonPointHandler : MonoBehaviour
+{
+    private void onClick()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClickManager.singleton.registerClickHandler(gameObject, onClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GG20/Assets/Scripts/ForestTilePlacer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestTilePlacer : MonoBehaviour
+{
+    private GameObject hitGO = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Find("Cube").GetComponent<Renderer>().material.SetColor("_Color", new Color(0, 1, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        LayerMask mask = LayerMask.GetMask("tiles");
+        RaycastHit[] hits = Physics.RaycastAll(ray, 100.0f, mask);
+        if (hits.Length > 0)
+        {
+            for (int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit hit = hits[i];
+                hitGO = hit.transform.gameObject;
+                transform.position = new Vector3(hitGO.transform.position.x, hitGO.transform.lossyScale.y, hitGO.transform.position.z);
+            }
+        }
+
+        if (hitGO != null && Input.GetMouseButtonDown(0))
+        {
+            Vector2Int pos = new Vector2Int((int)hitGO.transform.parent.position.x, (int)hitGO.transform.parent.position.z);
+            Debug.Log("Adding source at: " + pos);
+            GridManager.Singleton.TreeSources.Add(pos);
+            Destroy(gameObject);
+        }
+    }
+}
