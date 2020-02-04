a2076e1dafb039abdd0e6b49a99feb21fe8db8c1
AlonLivne <livnealon@gmail.com>
2020-02-04 21:26:39 +0200

    added tree generator for button

---
+++ GG20/Assets/TreeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGenerator : MonoBehaviour
+{
+    public GameObject TreePrefab;
+
+    public void OnClick()
+    {
+        if(PointManager.singleton.getPoints() > 100)
+        {
+            Instantiate(TreePrefab);
+        }
+ 
+    }
+}
